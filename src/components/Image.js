5373ab29313465bff5153cbfc8e8705652e3aa9f
simon <simonmassie95@live.be>
2020-10-05 15:54:37 +0200

    favorite

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,16 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
+import {Context} from "../Context"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 function Image({className, img}) {
     const [hovered, setHovered]= useState(false)
+    const {toggleFavorite} = useContext(Context)
     console.log(hovered)
     const elementHeart = <FontAwesomeIcon icon={faHeart} />
     const elementCart = <FontAwesomeIcon icon={faShoppingCart} />
-    const heartIcon = hovered && <i className="ri-heart-line favorite">{elementHeart}</i>
+    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}>{elementHeart}</i>
     const cartIcon = hovered && <i className="ri-add-circle-line cart">{elementCart}</i>
     
 
